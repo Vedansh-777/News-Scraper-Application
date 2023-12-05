444e4eab1d8ed1057969b5d91dc9431c2f71257e
Vedansh-777 <2020.vedansh.udhawani@ves.ac.in>
2023-12-06 02:27:21 +0530

    Done vercel settings

--- NewsAggregator/settings.py
+++ NewsAggregator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'xe4hmfwal+58e7@8u@&g^x2xsow_hyv7%m3wmk^o4u(%+!73cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
